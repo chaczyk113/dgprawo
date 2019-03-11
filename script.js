c0ec1a39044d6023c6afb027b2821333f0819ebf
Kuba <jakzacha@gmail.com>
2019-03-11 19:05:04 +0100

    mobile version start

--- script.js
+++ script.js
@@ -24,3 +24,43 @@ window.onload = ()=>{
          }
     })
 };
+
+function showMenu() {
+    var menuList = document.querySelector(".top-menu-links");
+    var menuButton = document.getElementById("menu-button");
+    var menuBar = document.querySelector("nav");
+    var menuLogo = document.querySelector(".logo img");
+
+    // console.log(menuList.style.display);
+    if (menuList.style.display == "flex") {
+        menuList.style.display = "none";
+    }
+    else {
+        menuList.style.display = "flex";
+    }
+}
+
+window.addEventListener('resize', function () {
+    resize_product_img();
+    resize_description_img();
+    move_dropdown();
+    restoreMenu();
+
+});
+
+
+function restoreMenu() {
+    var menuList = document.querySelector(".top-menu-links");
+    var menuButton = document.getElementById("menu-button");
+    var menuBar = document.querySelector("nav");
+    var menuLogo = document.querySelector(".logo img");
+
+    var w = window.innerWidth;
+    if (w > 1260) {
+        menuList.style.display = "";
+        menuButton.innerHTML = "MENU";
+        menuButton.classList.remove("button-black");
+        menuBar.classList.remove("bg-drop-menu");
+        menuLogo.src = "../images/logo-white.png"
+    }
+}
